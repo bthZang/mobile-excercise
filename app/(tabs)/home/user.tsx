e2f1f6a559f424d1eaa8fe026f99615e65b03656
Zang <bt.hoanggiang@gmail.com>
2024-03-28 22:47:22 +0700

    feat: login

--- app/(tabs)/home/user.tsx
+++ app/(tabs)/home/user.tsx
@@ -1,7 +1,39 @@
-import { View } from "react-native";
+import { useLocalSearchParams } from "expo-router";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 
-export default function UserScreen(){
-    return(
-        <View></View>
-    )
+export default function UserScreen() {
+  const user = useLocalSearchParams() as Record<string, any>;
+  user.subjects = user.subjects.split(",");
+
+  return (
+    <View style={styles.container}>
+      {user.subjects.map((subject: string) => (
+        <View style={styles.frame}>
+          <Text style={styles.item}>{subject}</Text>
+        </View>
+      ))}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+  },
+  frame: {
+    borderColor: "black",
+    borderWidth: 1,
+    width: "70%",
+    padding: 10,
+    marginBottom: 30,
+  },
+  item: {
+    fontSize: 18,
+    color: "#252520",
+    alignSelf: "center",
+  },
+});
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,36 +1,89 @@
-import { Button, StyleSheet, TextInput, View, Text } from 'react-native';
+import { Button, StyleSheet, TextInput, View, Text } from "react-native";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { router } from 'expo-router';
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { router } from "expo-router";
+import { useState } from "react";
 
 export default function LoginScreen() {
+  const users: any = [];
+  const subjects = [];
+  for (let i = 1; i <= 5; i++) {
+    subjects.push(`subject${i}`);
+    users.push({
+      username: `zang${i}`,
+      password: "1",
+      subjects: [...subjects],
+    });
+  }
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  // const [user, setUser] = useState({});
+  let user = {};
+
+  const handleLogin = (username: string, password: string) => {
+    for (let i = 0; i <= 4; i++)
+      if (username === users[i].username && password === users[i].password) {
+        user = users[i];
+        return true;
+      }
+    return false;
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Login</Text>
-      <View><TextInput ></TextInput></View>
-      
+      <View style={styles.textInput}>
+        <TextInput
+          onChangeText={setUsername}
+          autoFocus
+          placeholder="Username"
+        ></TextInput>
+      </View>
+      <View style={styles.textInput}>
+        <TextInput
+          onChangeText={setPassword}
+          autoFocus
+          placeholder="Password"
+        ></TextInput>
+      </View>
+
       <View style={styles.separator} />
-      <Button onPress={() => {
-        router.push('/(tabs)/home/user')
-      }} title='Login'></Button>
+      <Button
+        onPress={() => {
+          if (handleLogin(username, password))
+            router.push({ pathname: "/(tabs)/home/user", params: { ...user } });
+          else alert("invalid");
+        }}
+        title="Login"
+      ></Button>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "white",
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
-    color: 'red',
+    fontWeight: "bold",
+    color: "red",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
+  },
+  textInput: {
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 15,
+    width: "70%",
+    padding: 10,
+    marginBottom: 30,
   },
 });
